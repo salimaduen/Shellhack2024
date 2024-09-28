77a2c86048611987876305e23d918380a35582e2
BothLotus <hiramgtorres82@gmail.com>
2024-09-28 11:26:58 -0400

    Added dashboard

---
+++ frontend/main.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+import './main.css';
+const Dashboard = () => {
+    return (
+        <div className= "main-dashboard"  >
+            <header className="header-dashboard"> 
+                <img src= "logo.png" alt="logo" className="logo">
+                </img>
+            </header>
+        </div>  )
+
+}
