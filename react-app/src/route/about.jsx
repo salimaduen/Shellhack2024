4a58a2d077f711d130dd059fc4acce9eda45f88c
Isabella Schiappa Castro <xizzyxx@icloud.com>
2024-09-28 23:36:10 -0700

    added footer

--- react-app/src/route/about.jsx
+++ react-app/src/route/about.jsx
@@ -30,8 +30,11 @@ const About = () => {
       <div className="text-center mt-8">
         <button 
           className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-          onClick={() => window.location.href = '/login'}
+          onClick={event =>  window.location.href=''}
+
+          
         >
+          
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Sign Up
           </span>
--- react-app/src/route/footer.jsx
+++ react-app/src/route/footer.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-const Footer = () => {
-    return (
-      <footer>
-        <p>&copy; {new Date().getFullYear()} Your Company Name</p>
-      </footer>
-    );
+const Footer = () => { 
+  return (
+<section class="bg-white">
+    <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
+            <div class="px-5 py-2">
+                <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    About
+                </a>
+            </div>
+        </nav>
+        <p class="mt-8 text-base leading-6 text-center text-gray-400">
+            © 2024 MoneyMundo, Inc. All rights reserved.
+        </p>
+    </div>
+</section>
+  );
   };
 
 export default Footer;
+
+
